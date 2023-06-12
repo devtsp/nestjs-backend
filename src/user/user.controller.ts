a55d149bdc29d2b36ac1b6eb5b61ac4880e414d4
Tomas Paez <paeztms@gmail.com>
2023-06-11 23:50:51 -0300

    add edit user endpoint and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,31 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
+
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
+
   @Get('me')
   // @GetUser allow us to pass the already handled "request"
   // object as argument to the controller
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
