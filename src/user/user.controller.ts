e4369a15fda18d6adb629180f80bfd1cf545670c
Tomas Paez <paeztms@gmail.com>
2023-06-11 21:05:57 -0300

    add user route enpoint to retrieve info of user token, protected by auth strategy

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, UseGuards, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+
+@Controller('users')
+export class UserController {
+  // UseGuards will
+  @UseGuards(AuthGuard('jwt'))
+  @Get('me')
+  getMe(@Req() req: Request) {
+    console.log({ user: req.user });
+    return req.user;
+  }
+}
