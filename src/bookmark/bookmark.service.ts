f931e1152acac060172674261df10bd4fa327da4
Tomas Paez <paeztms@gmail.com>
2023-06-12 00:46:20 -0300

    add create and get all bookmark services/controllers

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,36 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from '@nestjs/common';
+
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+
+@Injectable()
+export class BookmarkService {
+  constructor(private prismaService: PrismaService) {}
+
+  getBookmarks(userId: number) {
+    return this.prismaService.bookmark.findMany({
+      where: { userId },
+    });
+  }
+
+  getBookmarkById(userId: number, bookmarkId: number) {}
+
+  async createBookmark(userId: number, dto: CreateBookmarkDto) {
+    const bookmark = await this.prismaService.bookmark.create({
+      data: {
+        userId,
+        ...dto,
+      },
+    });
+
+    return bookmark;
+  }
+
+  editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    dto: EditBookmarkDto,
+  ) {}
+
+  deleteBookmarkById(userId: number, bookmarkId: number) {}
+}
